997d84752b39b03efc7019fc2a6def19a0c4fca2
Olga Rozhkova <olga.gavrilova.og94@gmail.com>
2021-02-17 23:55:53 +0300

    createDreamTeam

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let cats = arr.flat();
+  let counter = 0;
+  cats.map(elem => elem === '^^' ? counter += 1: "it isn't cats");
+  return counter
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === false) {
+    return false;
+  } else {
+    let str = members.map(elem => (typeof(elem) === "string")? elem: '').toString();
+    for (let i = 0; i < str.length; i++) {
+      if(str[i] === ' ') {
+        str = str.replace(str[i], "").toUpperCase();
+        i = -1;
+      }
+    }
+  return str.split(',').map(elem => elem[0]).sort().join('').toUpperCase();
+  }
 };
